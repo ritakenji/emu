ae403fb481f3ba0c35b464da3f5e237b621c537f
Rita <rita.kenji@gmail.com>
2025-09-29 12:49:33 +0200

    instal and import SWRCOnfig and useSWR, send request GET on index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
+import useSWR from "swr";
+
 export default function HomePage() {
+  const { data, isLoading } = useSWR("/api/entries", { fallbackData: [] });
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+
   return (
     <div>
       <h1>Hello from Next.js</h1>
