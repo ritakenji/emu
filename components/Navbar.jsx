ab2f0c62d63dc0fcd4fd4dd2c43da23d867fb601
Rita <rita.kenji@gmail.com>
2025-09-29 11:24:21 +0200

    add png icon to navbar and style Nav component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,16 +1,27 @@
-import Link from "next/link"
-import styled from "styled-components"
+import Link from "next/link";
+import Image from "next/image";
+import styled from "styled-components";
 
-export default function NavBar () {
-    return <div>
-        <HomeLink href="/">
-        </HomeLink>
-    </div>
-   
+export default function NavBar() {
+  return (
+    <Navigation>
+      <HomeLink href="/">
+        <Image src={"/house.png"} alt="House image" height={30} width={30} />
+      </HomeLink>
+    </Navigation>
+  );
 }
 
-const HomeLink = styled(Link)`
+const Navigation = styled.div`
+  background-color: #8895c9;
+  display: flex;
+  justify-content: space-around;
+  position: fixed;
+  bottom: 0;
+  width: 100vw;
 `;
+
+const HomeLink = styled(Link)``;
 
 /* 
 
