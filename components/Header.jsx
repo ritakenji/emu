395d2afc0b3d7ef8ea48c2eb9eb703dd22924860
Rita <rita.kenji@gmail.com>
2025-09-29 11:45:20 +0200

    create header component, style it and import it to app

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default function Header() {
+  return <Title>Emu App</Title>;
+}
+
+const Title = styled.h1`
+  text-align: center;
+  font-size: 2em;
+`;
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import GlobalStyle from "../styles";
 import NavBar from "@/components/Navbar";
+import Header from "@/components/Header";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
+      <Header/>
       <Component {...pageProps} />
       <NavBar />
     </>
