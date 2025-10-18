455b629e3dc1b3496647589be9c1fa656336a2ae
Rita <rita.kenji@gmail.com>
2025-10-18 15:33:23 +0200

    style scroll back up button

--- components/Buttons/ScrollBackUpButton.jsx
+++ components/Buttons/ScrollBackUpButton.jsx
@@ -2,20 +2,20 @@ import { useState, useEffect } from "react";
 import { MoveUp } from "lucide-react";
 import styled from "styled-components";
 
-export default function ScrollBackUpButton () {
-    const [showScrollButton, setShowScrollButton] = useState(false);
+export default function ScrollBackUpButton() {
+  const [showScrollButton, setShowScrollButton] = useState(false);
 
-    const scrollToTop = () => {
+  const scrollToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth", // for a smooth scrolling effect
     });
   };
 
-    useEffect(() => {
+  useEffect(() => {
     const handleScroll = () => {
       // Show button if page scroll is more than 300px
-      if (window.scrollY > 300) { 
+      if (window.scrollY > 300) {
         setShowScrollButton(true);
       } else {
         setShowScrollButton(false);
@@ -34,8 +34,8 @@ export default function ScrollBackUpButton () {
   return (
     <>
       {showScrollButton && (
-        <ScrollButton onClick={scrollToTop}>
-          <MoveUp aria-hidden="true"/>Back to Top
+        <ScrollButton onClick={scrollToTop} aria-label="Scroll back to top">
+          <MoveUp aria-hidden="true" />
         </ScrollButton>
       )}
     </>
@@ -43,19 +43,18 @@ export default function ScrollBackUpButton () {
 }
 
 const ScrollButton = styled.button`
-  position: fixed; 
-  bottom: 110px; /* Adjust based on NavBar height */
-  right: 20px;
-  background-color: var(--color-primary); /* Use a color from your theme */
+  position: fixed;
+  bottom: 100px;
+  right: 25px;
+  background-color: var(--color-primary);
   color: white;
   border: none;
-  border-radius: 50%; /* Make it circular */
+  border-radius: 50%;
   width: 50px;
   height: 50px;
-  font-size: 1.5rem;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.08);
   cursor: pointer;
-  z-index: 1000; 
+  z-index: 1000;
   transition: opacity 0.3s;
 
   &:hover {
