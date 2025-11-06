169d8a43a81dba3a212b4bcf838c9ccf04b60a29
Britta <britta.3.maier@uni-konstanz.de>
2025-11-06 11:19:09 +0100

    Uncommented lines in upload.js in api folder

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -3,11 +3,11 @@ import formidable from "formidable";
 import fs from "fs";
 import os from "os";
 
-cloudinary.v2.config(/* {
+cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-} */);
+});
 export const config = {
   api: { bodyParser: false },
 };
